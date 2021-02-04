622dcb3f348f8a4eb45e42114c36eddd813098f4
Mark Brewer <markbrewer@Marks-MacBook-Pro.local>
2021-02-03 17:02:11 -0700

    add aditional fields for plain text inputs

--- client/public/vue/components/ReportCreator.vue
+++ client/public/vue/components/ReportCreator.vue
@@ -8,68 +8,74 @@
         <label for="activity">Activity</label>
         <select placeholder="select activity" name="activity" v-model="activity">
           <option class="placeholder">Select An Activity</option>
-          <option value="fish">Fish</option>
-          <option value="float">Float</option>
-          <option value="fish,float">Both</option>
-          <option value="other">Other</option>
+          <option :selected="user.activity === 'fish' ? 'selected' : ''" value="fish">Fish</option>
+          <option :selected="user.activity === 'float' ? 'selected' : ''" value="float">Float</option>
+          <option :selected="user.activity === 'both' ? 'selected' : ''" value="fish,float">Both</option>
+          <option :selected="user.activity === 'other' ? 'selected' : ''" value="other">Other</option>
         </select>
         <label v-if="activity.includes('other')" for="activitywritein">Activity</label>
-        <input v-if="activity.includes('other')" type="activitywritein" name="activitywritein" placeholder="write activity here" v-model="activitywritein">
-        <label v-if="activity.includes('fish')" for="numcaught">Number of Fish Caught</label>
-        <input v-if="activity.includes('fish')" type="number" name="numcaught" min="0" value="0" v-model="numCaught">
-        <div v-if="activity.includes('fish')" class="item-editor">
-          <button class="button button-green --narrow --hollow" v-on:click="addFish" type="button" name="add fish">+ Add Fish Description</button>
-          <div v-for="(fish, index) in allFish" :key="fish.id" class="item-editor__inputs">
-            <button class="button button-red --circular" v-on:click="removeFish(index)" type="button">x</button>
-            <span v-if="!fish.opened" class="collapsed-label">
-              Species: {{fish.species}} | Length: {{fish.length}} | Weight: {{fish.weight}}
-            </span>
-            <button class="button button-blue --inline" v-on:click="fish.opened = !fish.opened" type="button" aria-label="expand fish">{{ fish.opened? 'Collapse ˄' : 'Expand ˅'}}</button>
-            <fieldset :class="{'--hidden': !fish.opened}">
-              <label for="species">Species</label>
-              <select placeholder="select species" name="species" v-on:input="setFishField(index, 'species', $event)">
-                <option class="placeholder">Select a species</option>
-                <option v-for="s in species" v-bind:value="s.name">{{s.name}}</option>
-                <option value="other">Other</option>
-              </select>
-              <label v-if="fish.species === 'other'" for="specieswritein">Species Write In</label>
-              <input v-if="fish.species === 'other'" type="text" name="specieswritein" v-on:input="setFishField(index, 'specieswritein', $event)" placeholder="write species here">
-              <label for="length">Length</label>
-              <input type="number" min="0" name="length" v-on:input="setFishField(index, 'length', $event)">
-              <label for="weight">Weight</label>
-              <input type="number" min="0" name="weight" v-on:input="setFishField(index, 'weight', $event)">
-            </fieldset>
-          </div>
+        <input v-if="activity.includes('other')" type="text" name="activitywritein" placeholder="write activity here" v-model="activitywritein">
+        <div class="section-header" v-if="activity.includes('fish')">
+          <h3>Fishing Info</h3><button @click="showFishInfo = !showFishInfo" class="button button-blue --inline" type="button" aria-label="expand boat info">{{ showFishInfo ? 'Collapse ˄' : 'Expand ˅'}}</button>
         </div>
-        <div v-if="activity.includes('fish')" class="item-editor">
-          <button class="button button-green --narrow --hollow" v-on:click="addFly" type="button" name="add fly">+ Add Fly Description</button>
-          <div v-for="(fly, index) in allFlys" :key="fly.id" class="item-editor__inputs">
-            <button class="button button-red --circular" v-on:click="removeFly(index)" type="button">x</button>
-            <span v-if="!fly.opened" class="collapsed-label">
-              Name: {{fly.name}} | color: {{fly.color}} | size: {{fly.size}}
-            </span>
-            <button class="button button-blue --inline" v-on:click="fly.opened = !fly.opened" type="button" aria-label="expand fly">{{ fly.opened? 'Collapse ˄' : 'Expand ˅'}}</button>
-            <fieldset :class="{'--hidden': !fly.opened}">
-              <label for="method">Type</label>
-              <select placeholder="select type" type="method" name="method" v-on:input="setFlyField(index, 'method', $event)">
-                <option class="placeholder">Select A Type</option>
-                <option value="nymph">Nymph</option>
-                <option value="emerger">Emerger</option>
-                <option value="dry">Dry Fly</option>
-                <option value="terrestrial">Terrestrial(hopper, ants, etc)</option>
-                <option value="streamer">Streamer</option>
-                <option value="stimulator">Stimulator</option>
-              </select>
-              <label for="name">Name</label>
-              <input type="text" name="name" v-on:input="setFlyField(index, 'name', $event)">
-              <label for="color">Color</label>
-              <input type="text" name="color" v-on:input="setFlyField(index, 'color', $event)">
-              <label for="size">Size</label>
-              <input type="number" name="size" v-on:input="setFlyField(index, 'size', $event)">
-            </fieldset>
+        <fieldset v-if="activity.includes('fish') && showFishInfo">
+          <div v-if="activity.includes('fish')" class="item-editor">
+            <button class="button button-green --narrow --hollow" v-on:click="addFish" type="button" name="add fish">+ Add Fish Description</button>
+            <div v-for="(fish, index) in allFish" :key="fish.id" class="item-editor__inputs">
+              <button class="button button-red --circular" v-on:click="removeFish(index)" type="button">x</button>
+              <span v-if="!fish.opened" class="collapsed-label">
+                Species: {{fish.species}}
+              </span>
+              <button class="button button-blue --inline" v-on:click="fish.opened = !fish.opened" type="button" aria-label="expand fish">{{ fish.opened? 'Collapse ˄' : 'Expand ˅'}}</button>
+              <fieldset v-if="fish.opened">
+                <label for="species">Species</label>
+                <select placeholder="select species" name="species" v-on:input="setFishField(index, 'species', $event)">
+                  <option class="placeholder">Select a species</option>
+                  <option v-for="s in species" v-bind:value="s.name">{{s.name}}</option>
+                  <option value="other">Other</option>
+                </select>
+                <label v-if="fish.species === 'other'" for="specieswritein">Species Write In</label>
+                <input v-if="fish.species === 'other'" type="text" name="specieswritein" v-on:input="setFishField(index, 'specieswritein', $event)" placeholder="write species here">
+                <label for="length">Length</label>
+                <input type="number" min="0" name="length" v-on:input="setFishField(index, 'length', $event)">
+                <label for="weight">Weight</label>
+                <input type="number" min="0" name="weight" v-on:input="setFishField(index, 'weight', $event)">
+              </fieldset>
+            </div>
           </div>
+          <div v-if="activity.includes('fish')" class="item-editor">
+            <button class="button button-green --narrow --hollow" v-on:click="addFly" type="button" name="add fly">+ Add Fly Description</button>
+            <div v-for="(fly, index) in allFlys" :key="fly.id" class="item-editor__inputs">
+              <button class="button button-red --circular" v-on:click="removeFly(index)" type="button">x</button>
+              <span v-if="!fly.opened" class="collapsed-label">
+                Name: {{fly.name}} | color: {{fly.color}} | size: {{fly.size}}
+              </span>
+              <button class="button button-blue --inline" v-on:click="fly.opened = !fly.opened" type="button" aria-label="expand fly">{{ fly.opened? 'Collapse ˄' : 'Expand ˅'}}</button>
+              <fieldset v-if="fly.opened">
+                <label for="method">Type</label>
+                <select placeholder="select type" type="text" name="method" v-on:input="setFlyField(index, 'method', $event)">
+                  <option class="placeholder">Select A Type</option>
+                  <option value="nymph">Nymph</option>
+                  <option value="emerger">Emerger</option>
+                  <option value="dry">Dry Fly</option>
+                  <option value="terrestrial">Terrestrial(hopper, ants, etc)</option>
+                  <option value="streamer">Streamer</option>
+                  <option value="stimulator">Stimulator</option>
+                </select>
+                <label for="name">Name</label>
+                <input type="text" name="name" v-on:input="setFlyField(index, 'name', $event)">
+                <label for="color">Color</label>
+                <input type="text" name="color" v-on:input="setFlyField(index, 'color', $event)">
+                <label for="size">Size</label>
+                <input type="number" name="size" v-on:input="setFlyField(index, 'size', $event)">
+              </fieldset>
+            </div>
+          </div>
+        </fieldset>
+        <div class="section-header" v-if="activity.includes('float')">
+          <h3>Boat Info</h3><button @click="showBoatInfo = !showBoatInfo" class="button button-blue --inline" type="button" aria-label="expand boat info">{{ showBoatInfo ? 'Collapse ˄' : 'Expand ˅'}}</button>
         </div>
-        <fieldset v-if="activity.includes('float')">
+        <fieldset v-if="activity.includes('float') && showBoatInfo">
           <label for="watercraft">Boat Type</label>
           <select placeholder="select type" v-if="activity.includes('float')" name="watercraft" v-model="watercraft">
             <option class="placeholder">Select A Type</option>
@@ -83,13 +89,18 @@
             <option value="other">Other</option>
           </select>
           <label v-if="watercraft === 'other'" for="watercraftwritein">Boat Type Write In</label>
-          <input v-if="watercraft === 'other'" type="watercraftwritein" name="watercraftwritein" placeholder="write boat type here (EG: kayak, canoe, raft)" v-model="watercraftwritein">
+          <input v-if="watercraft === 'other'" type="text" name="watercraftwritein" placeholder="write boat type here (EG: kayak, canoe, raft)" v-model="watercraftwritein">
           <label for="watercraftmake">Boat Make</label>
-          <input type="watercraftmake" name="watercraftmake" placeholder="write boat make here (EG: NRS, Clackacraft, Kokatat)" v-model="watercraftmake">
+          <input type="text" name="watercraftmake" placeholder="write boat make here (EG: NRS, Clackacraft, Kokatat)" v-model="watercraftmake">
           <label for="watercraftmodel">Boat Model</label>
-          <input type="watercraftmodel" name="watercraftmodel" placeholder="write boat model here" v-model="watercraftmodel">
+          <input type="text" name="watercraftmodel" placeholder="write boat model here" v-model="watercraftmodel">
+          <label for="watercraftlength">Boat Length</label>
+          <input type="number" name="watercraftlength" placeholder="write boat model here" v-model="watercraftlength">
         </fieldset>
-        <fieldset v-if="activity.includes('float')">
+        <div class="section-header" v-if="activity.includes('float')">
+          <h3>Launch Info</h3><button @click="showLaunchInfo = !showLaunchInfo" class="button button-blue --inline" type="button" aria-label="expand boat info">{{ showLaunchInfo ? 'Collapse ˄' : 'Expand ˅'}}</button>
+        </div>
+        <fieldset v-if="activity.includes('float') && showLaunchInfo">
           <label for="putInName">Put In Name</label>
           <input  name="putInName" v-model="putInName"></input>
           <label for="putInLocation">Put In Location</label>
@@ -132,6 +143,7 @@
         comment: null,
         watercraft: null,
         watercraftwritein: null,
+        watercraftlength: null,
         watercraftmake: null,
         watercraftmodel: null,
         putInName: null,
@@ -140,9 +152,11 @@
         takeOutLocation: null,
         activity: [],
         activitywritein: null,
-        numCaught: null,
         species: species,
         isPrivate: false,
+        showFishInfo: true,
+        showBoatInfo: true,
+        showLaunchInfo: true,
         pointSelectorOpen: false,
         pointSelectorContext: null
       }
@@ -160,7 +174,7 @@
       addFish() {
         const id = Math.random().toString(36).substring(7);
         this.allFish.forEach(f => f.opened = false);
-        this.allFish.push({id, species: null, length: null, weight: null, opened: true})
+        this.allFish.push({id, species: null, specieswritein: null, length: null, weight: null, opened: true})
       },
       setFlyField(i, field, event) {
         this.allFlys[i][field] = event.target.value;
@@ -191,16 +205,21 @@
         console.log({
           title: this.title,
           author: this.user._id,
-          activity: this.activitywritein ? [this.activitywritein] : this.activity.split(','),
-          numCaught: this.numCaught,
+          activity: this.activity.length > 1 ? 'both' : this.activity.length ? this.activity[0] : 'other',
+          activitywritein: this.activitywritein,
+          numCaught: this.allFish.length,
           fish: this.allFish,
           flys: this.allFlys,
           comment: this.comment,
-          watercraft: this.watercraftwritein ? this.watercraftwritein : this.watercraft,
+          watercraft: this.watercraft,
+          watercraftwritein: this.watercraftwritein,
           watercraftmake: this.watercraftmake,
           watercraftmodel: this.watercraftmodel,
-          putIn: this.putIn,
-          takeOut: this.takeOut,
+          watercraftlength: this.watercraftlength,
+          putInName: this.putInName,
+          putInLocation: this.putInLocation,
+          takeOutName: this.takeOutName,
+          takeOutLocation: this.takeOutLocation,
           isPrivate: this.isPrivate
         })
       }
